56cdc0fdb2035ad7abf5f20e6397bd2afc097f08
Daniel Wykerd <danielwykerd@gmail.com>
2021-08-02 19:04:57 +0200

    feat(struct): helper methods

--- include/lldc/structure.h
+++ include/lldc/structure.h
@@ -47,13 +47,80 @@ typedef struct lldc_struct_obj_root_s {
     lldc_struct_malloc_ledger_t __mlog;
 } lldc_struct_obj_root_t;
 
-void *lldc_struct_strdup (lldc_struct_obj_t *parser, const char *str);
-void *lldc_struct_malloc (lldc_struct_obj_t *parser, size_t size);
-void lldc_struct_free (lldc_struct_obj_t *parser);
+/* Base array type */
+typedef struct lldc_struct_opaque_arr_s {
+    LLDC_STRUCT_BASE
+    void *items;
+    size_t len;
+} lldc_struct_opaque_arr_t;
+
+typedef struct lldc_struct_opaque_root_arr_s {
+    LLDC_STRUCT_BASE
+    void *items;
+    size_t len;
+    lldc_struct_malloc_ledger_t __mlog;
+} lldc_struct_opaque_root_arr_t;
+
+/**
+ * Allocate and copy a string as a member of a structure
+ */
+void *lldc_struct_strdup (lldc_struct_obj_t *st, const char *str);
+/**
+ * Allocate memory owned by the structure 
+ */
+void *lldc_struct_malloc (lldc_struct_obj_t *st, size_t size);
+/**
+ * Reallocate memory already owned by the structure 
+ */
+void *lldc_struct_realloc (lldc_struct_obj_t *st, void *ptr, size_t size);
+/**
+ * Free a structure and all of its members
+ */
+void lldc_struct_free (lldc_struct_obj_t *st);
 /**
  * Initialize a structure for use without parsing from json
  */
-void lldc_struct_init (cwr_malloc_ctx_t *_mctx, lldc_struct_obj_root_t *parser);
+void lldc_struct_init (cwr_malloc_ctx_t *_mctx, lldc_struct_obj_root_t *st);
+/**
+ * Set the value of a string member
+ * 
+ * @param ptr pointer to the member to set
+ * @param val the value to copy (strdup)
+ * @return NULL if fail else pointer to the newly set value
+ */
+const char *lldc_struct_set_str (lldc_struct_obj_t *st, const char **ptr, const char *val);
+/**
+ * Set the value of a string member
+ * 
+ * @param ptr pointer to the member to set
+ * @param val the value to copy (strdup)
+ * @param len length of the string
+ * @return NULL if fail else pointer to the newly set value
+ */
+const char *lldc_struct_set_strn (lldc_struct_obj_t *st, const char **ptr, const char *val, size_t len);
+/**
+ * Initialize array not previously initialized by a parser
+ * 
+ * @param st parent structure (this structure owns all the pointers allocated by the array)
+ * @param arr the array
+ */
+void lldc_struct_init_arr (lldc_struct_obj_t *st, lldc_struct_opaque_arr_t *arr);
+/**
+ * Initialize root array not previously initialized by a parser
+ * 
+ * @param arr the array
+ */
+void lldc_struct_init_root_arr (cwr_malloc_ctx_t *_mctx, lldc_struct_opaque_root_arr_t *arr);
+/**
+ * Allocate several new elements in an array
+ * 
+ * @param arr the array structure to use
+ * @param elem_size size of the array element
+ * @param count amount of elements to allocate
+ * @return Pointer to the new element
+ */
+void *lldc_struct_add_elem_arr (lldc_struct_opaque_arr_t *arr, size_t elem_size, size_t count);
+
 
 /* Primatives Arrays */
 typedef struct lldc_struct_string_arr_s {
@@ -90,14 +157,22 @@ typedef struct lldc_struct_snowflake_arr_s {
     size_t len;
 } lldc_struct_snowflake_arr_t;
 
+/// Used internally
 int lldc_struct_int_arr_parse (lldc_struct_int_arr_t *arr, yyjson_val *json);
+/// Used internally
 int lldc_struct_uint_arr_parse (lldc_struct_uint_arr_t *arr, yyjson_val *json);
+/// Used internally
 int lldc_struct_string_arr_parse (lldc_struct_string_arr_t *arr, yyjson_val *json);
+/// Used internally
 int lldc_struct_toint_arr_parse(lldc_struct_uint_arr_t *arr, yyjson_val *json);
+/// Used internally
 int lldc_struct_double_arr_parse(lldc_struct_double_arr_t *arr, yyjson_val *json);
+/// Used internally
 int lldc_struct_timestamp_arr_parse(lldc_struct_timestamp_arr_t *arr, yyjson_val *json);
 
+/// Used internally
 #define lldc_struct_boolean_arr_parse(arr, json) lldc_struct_int_arr_parse((lldc_struct_int_arr_t *)arr, json)
+/// Used internally
 #define lldc_struct_snowflake_arr_parse(arr, json) lldc_struct_toint_arr_parse((lldc_struct_uint_arr_t *)arr, json)
 
 #endif
--- src/structure.c
+++ src/structure.c
@@ -9,6 +9,7 @@
 #include <lldc/structure.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define LLDC_PARSER_LEDGER_DEF_SIZE 8
 
@@ -41,6 +42,12 @@ void *lldc_struct_malloc (lldc_struct_obj_t *parser, size_t size)
     return ptr;
 }
 
+void *lldc_struct_realloc (lldc_struct_obj_t *st, void *ptr, size_t size)
+{
+    assert(ptr != NULL && "Pointer must not be null");
+    return cwr_realloc(st->_mctx, ptr, size);
+}
+
 void *lldc_struct_strdup (lldc_struct_obj_t *parser, const char *str)
 {
     size_t len = strlen(str);
@@ -54,6 +61,7 @@ void *lldc_struct_strdup (lldc_struct_obj_t *parser, const char *str)
 
 void lldc_struct_init (cwr_malloc_ctx_t *_mctx, lldc_struct_obj_root_t *parser)
 {
+    parser->_mctx = _mctx;
     parser->_mlog = &parser->__mlog;
 }
 
@@ -67,6 +75,71 @@ void lldc_struct_free (lldc_struct_obj_t *parser)
     parser->_mlog->pointers = NULL;
     parser->_mlog->len = 0;
     parser->_mlog->size = 0;
+}
+
+const char *lldc_struct_set_strn (lldc_struct_obj_t *st, const char **ptr, const char *val, size_t len)
+{
+    if (*ptr == NULL)
+    {
+        *ptr = lldc_struct_malloc(st, len);
+        if (*ptr == NULL)
+            return NULL;
+        memcpy(*ptr, val, len);
+        return *ptr;
+    }
+
+    char *str = lldc_struct_realloc(st, *ptr, len);
+    if (*str = NULL)
+        return NULL;
+    
+    *ptr = str;
+    memcpy(*ptr, val, len);
+
+    return *ptr;
+}
+
+const char *lldc_struct_set_str (lldc_struct_obj_t *st, const char **ptr, const char *val)
+{
+    if (*ptr == NULL)
+    {
+        *ptr = lldc_struct_strdup(st, val);
+        return *ptr;
+    }
+
+    return lldc_struct_set_strn(st, ptr, val, strlen(val) + 1);
+}
+
+void lldc_struct_init_arr (lldc_struct_obj_t *st, lldc_struct_opaque_arr_t *arr)
+{
+    arr->_mctx = st->_mctx;
+    arr->_mlog = st->_mlog;
+}
+
+void lldc_struct_init_root_arr (cwr_malloc_ctx_t *_mctx, lldc_struct_opaque_root_arr_t *parser)
+{
+    parser->_mctx = _mctx;
+    parser->_mlog = &parser->__mlog;
+}
+
+void *lldc_struct_add_elem_arr (lldc_struct_opaque_arr_t *arr, size_t elem_size, size_t count)
+{
+    if (arr->items == NULL)
+    {
+        arr->items = lldc_struct_malloc((lldc_struct_obj_t *)arr, elem_size * count);
+        memset(arr->items, 0, elem_size * count);
+        arr->len = count;
+        return arr->items;
+    }
+
+    char *n_arr = lldc_struct_realloc((lldc_struct_obj_t *)arr, arr->items, (arr->len + count) * elem_size);
+    if (*n_arr = NULL)
+        return NULL;
+
+    arr->items = n_arr;
+    memset(arr->items + arr->len, 0, (arr->len + count) * elem_size);
+    arr->len += count;
+
+    return arr->items + arr->len - count;
 }
 
 int lldc_struct_int_arr_parse (lldc_struct_int_arr_t *arr, yyjson_val *json)
